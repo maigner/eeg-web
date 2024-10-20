ada4285b5a6485e96c9ac94de8b7f471ed0fc3fd
Martin Aigner <martin@maigner.net>
2024-10-20 21:29:37 +0200

    flowbite

---
+++ client/db/README.md
@@ -0,0 +1,1 @@
+# postgres instance for client app
--- client/webapp/src/routes/+page.svelte
+++ client/webapp/src/routes/+page.svelte
@@ -1,5 +1,14 @@
+<script>
+    import { Alert } from "flowbite-svelte";
+</script>
+
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Achtung!</span>
+        Es ist noch keine Datenbank konfiguriert.
+    </Alert>
+</div>
+
 <h1>Ihre neue Energiegemeinschaft</h1>
 
-<p>
-    steht für Sie bereit
-</p>
+<p>steht für Sie bereit</p>
